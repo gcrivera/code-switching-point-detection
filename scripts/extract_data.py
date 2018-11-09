17c221eb5c1dfe348168f647b804a053006ac971
Gabrielle Rivera <gcrivera@mit.edu>
2018-11-09 15:11:33 -0500

    Added comments with instructions for extracting data

---
+++ scripts/extract_data.py
@@ -0,0 +1,9 @@
+# Create npy files - train_non_switch.npy, train_switch.npy, test_non_switch.npy, test_switch.npy
+# try 75 ms, 100 ms, 150 ms windows
+# TODO: Read through text.bw
+# For each utterance decide where switch points are using word_align
+# Figure out where file is located and test or train
+# extrace MFCC features
+# normalize
+# put into correct numpy arrays
+# write numpy arrays to disk
