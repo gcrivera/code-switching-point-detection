f07605ba09a95223795464428df45a31c066f280
Gabrielle Rivera <gcrivera@mit.edu>
2018-11-15 12:17:42 -0500

    adding print statements back in

--- scripts/data.py
+++ scripts/data.py
@@ -146,6 +146,9 @@ def extract(window, num_features):
             elif word == '<non-MSA>' and i == 0:
                 continue
             else:
+                print alignment_identifier
+                print alignment_data
+                print utterance_words
                 word_data = alignment_data.pop(0)
                 if record_switch:
                     # Add features for switch point
