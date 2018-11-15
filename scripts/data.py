369ac491c6eb7bcadca25edbeb312cda4c11162f
Gabrielle Rivera <gcrivera@mit.edu>
2018-11-15 12:27:14 -0500

    adding debugging statements back in

--- scripts/data.py
+++ scripts/data.py
@@ -146,6 +146,9 @@ def extract(window, num_features):
             elif word == '<non-MSA>' and i == 0:
                 continue
             else:
+                print alignment_identifier
+                print utterance_words
+                print alignment_data
                 word_data = alignment_data.pop(0)
                 if record_switch:
                     # Add features for switch point
