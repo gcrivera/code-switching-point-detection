8ac5151bfede360d6c57b7577abead2a784fa65b
Gabrielle Rivera <gcrivera@mit.edu>
2018-11-15 12:39:27 -0500

    debugging

--- scripts/data.py
+++ scripts/data.py
@@ -147,6 +147,9 @@ def extract(window, num_features):
             elif word == '<non-MSA>' and i == 0:
                 continue
             else:
+                print alignment_identifier
+                print alignment_data
+                print utterance_words[i:]
                 word_data = alignment_data.pop(0)
                 if record_switch:
                     # Add features for switch point
